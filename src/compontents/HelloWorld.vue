39553a9c259aaa3179e37d8ebe8b09d6c817a055
wjswlile <1694250354@qq.com>
2018-11-10 19:47:24 +0800

    Vuezuoye

---
+++ src/compontents/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{msg}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+        msg: '呵呵!!!'
+    }
+  }
+</script>
+
+<style>
+  h2 {
+  color: red;
+}
+
+</style>
